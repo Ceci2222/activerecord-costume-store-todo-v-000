727c4e4dfb01105c424fc118f9e6e625c1f52a00
Marie Burns <mcburns2222@gmail.com>
2019-02-16 01:46:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :business
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.time :opening_time
       t.time :closing_time
     end
