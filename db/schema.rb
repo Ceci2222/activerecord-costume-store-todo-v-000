290f703537c868a75606a60d91abbaf58016ab77
Marie Burns <mcburns2222@gmail.com>
2019-02-16 01:50:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
+    t.integer "number_of_employees"
+    t.boolean "business"
     t.time "opening_time"
     t.time "closing_time"
   end
@@ -29,17 +29,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.date "opening_date"
-    t.date "closing_date"
-    t.string "description"
   end
 
 end
